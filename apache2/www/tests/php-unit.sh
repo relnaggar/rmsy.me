b024a89f8c496509c983e2b201192deae78758f5
Ramsey144 <ramsey144@gmail.com>
2020-04-21 05:03:01 +0100

    Test suite working in production

--- apache2/www/tests/php-unit.sh
+++ apache2/www/tests/php-unit.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/
 composer dumpautoload
-/home/apache2/.composer/vendor/bin/phpunit --bootstrap vendor/autoload.php --testdox tests
+/home/apache2/.composer/vendor/bin/phpunit --bootstrap vendor/autoload.php --testdox tests --do-not-cache-result
 RESULT=$?
 rm -r vendor
 exit $RESULT
--- development.sh
+++ development.sh
@@ -9,6 +9,9 @@ elif [ "$1" = "down" ]; then
 elif [ "$1" = "logs" ]; then
     docker service logs rmsy-me_apache2
     docker service logs rmsy-me_postgres
+elif [ "$1" = "bash" ] && [ "$#" -eq 2 ]; then
+    CONTAINER=`docker ps -a | grep -o "rmsy-me_$2.*"`
+    exec docker exec -it $CONTAINER bash
 else
     echo "operation not yet supported"
 fi
