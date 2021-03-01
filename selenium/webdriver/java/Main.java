12e5deb5a81d5efeaf47f33a37824d4df7850ae9
relnaggar <rmsy@rmsy.me>
2021-03-01 19:38:37 +0000

    Add script for updating resume from overleaf

--- selenium/webdriver/java/Main.java
+++ selenium/webdriver/java/Main.java
@@ -1,44 +1,58 @@
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import java.net.URL;
-import java.net.MalformedURLException;
+import java.util.*;
+import java.util.concurrent.*;
+import java.io.*;
+import java.nio.file.*;
+import java.time.*;
+import java.net.*;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 class Main {
-  public static void main(String[] args) throws MalformedURLException, InterruptedException {
-      System.out.println("Starting...");
+  private static WebDriver driver;
 
-      ChromeOptions chromeOptions = new ChromeOptions();
-      chromeOptions.addArguments("--ignore-certificate-errors");
-      WebDriver driver = new RemoteWebDriver(new URL("http://selenium-chrome:4444/wd/hub"), chromeOptions);
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  public static void startWebDriver() throws MalformedURLException {
+    System.out.println("Starting web driver");
 
-      driver.manage().window().maximize();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--ignore-certificate-errors");
+    driver = new RemoteWebDriver(new URL("http://selenium-chrome:4444/wd/hub"), chromeOptions);
 
-      driver.get("https://www.overleaf.com/read/kpqmdmzrsrvn");
-      TimeUnit.SECONDS.sleep(10);
+    driver.manage().window().maximize();
 
-      // get href attribute of parent of element with class "fa fa-fw fa-download"
-      WebElement i = driver.findElement(By.xpath("//*[@class='fa fa-fw fa-download']"));
-      i.click();
-      //WebElement a = i.findElement(By.xpath("../."));
-      //String downloadLink = a.getAttribute("href");
-      //System.out.println("Download link: " + downloadLink);
-      //downloadLink = downloadLink.substring(0, downloadLink.length() - "&popupDownload=true".length());
-      //System.out.println("Without popup: " + downloadLink);
+    System.out.println("Web driver started");
+  }
 
-      // Get All available cookies
-      //Set<Cookie> cookies = driver.manage().getCookies();
-      //for (Cookie cookie: cookies) {
-        //System.out.println(cookie.toJson());
-      //}
+  public static void endWebDriver() {
+    System.out.println("Stopping web driver");
+    driver.quit();
+    System.out.println("Web driver stopped");
+  }
 
-      TimeUnit.SECONDS.sleep(10);
+  public static void downloadOverleafPdf(String overleafUrl) throws IOException, InterruptedException {
+    System.out.println("GET " + overleafUrl);
+    driver.get(overleafUrl);
+    String xPath = "//*[@class='fa fa-fw fa-download']";
 
-      driver.quit();
+    System.out.println("Waiting for download link to be clickable");
+    new FluentWait<WebDriver>(driver)
+      .withTimeout(Duration.ofSeconds(10))
+      .pollingEvery(Duration.ofSeconds(1))
+      .ignoring(StaleElementReferenceException.class)
+      .ignoring(org.openqa.selenium.NoSuchElementException.class)
+      .until(ExpectedConditions.attributeContains(By.xpath(xPath + "/.."), "href","https://www.overleaf.com/download/project/"));
+
+    System.out.println("Downloading PDF");
+    driver.findElement(By.xpath(xPath)).click();
+
+    TimeUnit.SECONDS.sleep(5);
+  }
+  
+  public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException {
+    startWebDriver();
+    downloadOverleafPdf("https://www.overleaf.com/read/kpqmdmzrsrvn");
+    endWebDriver();
   }
 }
