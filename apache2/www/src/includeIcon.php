4d5ec907fdde58b72574add5176c71027dac0ce8
relnaggar <rmsy@rmsy.me>
2021-04-04 20:31:35 +0100

    Cleanup

--- apache2/www/src/includeIcon.php
+++ apache2/www/src/includeIcon.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 $icons = [];
-function includeIcon($name) {
+function includeIcon(string $name): string {
   global $icons;
   if (!isset($icons[$name])) {
     $icons[$name] = loadTemplate('/icons/' . $name, $templateVars=[], $fileExtension='.svg');
