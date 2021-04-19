c6d2ae1e9593d2dfe815956d4080580afccf1951
relnaggar <rmsy@rmsy.me>
2021-04-19 13:46:21 +0100

    [RM-18] server-side reCAPTCHA validation

--- apache2/www/src/Controllers/Engineer.php
+++ apache2/www/src/Controllers/Engineer.php
@@ -29,7 +29,7 @@ class Engineer {
     ];
   }
 
-  private function recaptcha(): array {
+  private function getRecaptchaDetails(): array {
     $recaptcha['nonce'] = base64_encode(random_bytes(18)); // number of bytes must be a multiple of 3 greater than 16
     if (isset($GLOBALS['PRODUCTION'])) {
       $recaptcha['sitekey'] = '***REMOVED***';
@@ -48,10 +48,55 @@ class Engineer {
     return $this->basic(__FUNCTION__);
   }
 
+  private function sendPostRequest(string $url, array $data) {
+    $options = [
+      'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+      ],
+      'ssl' => [
+        'cafile' => '../cacert.cer',
+      ]
+    ];
+    return file_get_contents($url, false, stream_context_create($options));
+  }
+
+  private function validateRecaptcha() {
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = [
+      'response' => $_POST['g-recaptcha-response']
+    ];
+    if (isset($GLOBALS['PRODUCTION'])) {
+      $data['secret'] = '***REMOVED***';
+    } else {                                                     
+      $data['secret'] = '***REMOVED***';
+    }
+    return $this->sendPostRequest($url, $data);
+  }
+
   public function contact(): array {
-    return $this->basic(__FUNCTION__, [
-        'recaptcha' => $this->recaptcha(),
-    ]);
+    $message['sent'] = false;
+    if (isset($_POST['submit'])) {
+      $result_string = $this->validateRecaptcha();
+      if ($result_string) {
+        $result = json_decode($result_string, true);
+        if ($result['success']) {
+          $message['sent'] = true;
+        } else if (count($result['error-codes']) === 1 && $result['error-codes'][0] === 'timeout-or-duplicate') {
+          $message['error-code'] = 'timeout-or-duplicate';
+        } else {
+          $message['error-code'] = 'internal';
+        }
+      } else {
+        $message['error-code'] = 'internal';
+      }
+    }
+    if (!$message['sent']) {
+      $vars['recaptcha'] = $this->getRecaptchaDetails();
+    }
+    $vars['message'] = $message;
+    return $this->basic(__FUNCTION__, $vars);
   }
 
   public function pageNotFound(): array {
--- apache2/www/src/templates/engineer/contact.html.php
+++ apache2/www/src/templates/engineer/contact.html.php
@@ -1,51 +1,76 @@
 <p class="lead">Any questions, feedback or general enquiries are welcome. To get in touch, you can send me an email or submit the contact form below. You can also send me an invitation on Linkedin.</p>
 
-<script src="/assets/contact-form.js" async defer></script>
-<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback" async defer nonce="<?=$recaptcha['nonce']?>"></script>
-
 <div class="row">
   <div class="col-xl-8 mb-3">
-    <div class="card">
-      <div class="card-header">
-        <h6 class="mb-0">Contact form</h6>
+    <?php if ($message['sent']) { ?>
+      <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Message sent!</h4>
+        <p>
+          Your message has been sent successfully.
+          As confirmation, a copy of your message has also been forwarded to your email address.
+        </p>
       </div>
-      <div class="card-body">
-        <form action="/engineer/contact" method="POST" autocomplete="on" class="needs-validation" novalidate>
-          <!-- TODO: value, autofocus -->
-          <div class="mb-3">
-            <label for="name" class="col-form-label">Name</label>
-            <input type="text" class="form-control" id="name" name="message['name']" required>
-            <div class="invalid-feedback">
-              This field cannot be blank.
+    <?php } else { ?>
+      <?php if (isset($message['error-code'])) { ?>
+        <div class="alert alert-danger" role="alert">
+          <h4 class="alert-heading">Message send failure</h4>
+            <?php if ($message['error-code'] === 'timeout-or-duplicate') { ?>
+              <p>
+                It looks like this was a form resubmission which has failed the reCAPTCHA validation. Try manually resubmitting the form below. You can do this by checking the reCAPTCHA box and pressing the Submit button.
+              </p>
+            <?php } else { ?>
+              <p>
+              It looks like there was some kind of internal error when trying to validate your message with reCAPTCHA or when trying to send your message as an email. This might just be a one-off problem, so try submitting your message again using the form below.
+              </p>
+              <p>
+                If you keep seeing this error message, it probably indicates that the contact form is no longer working. Sorry about this! The best thing to do is copy the message that you've written below so you don't lose it and then send it to me in an email instead.
+              </p>
+            <?php } ?>
+        </div>
+      <?php } ?>
+      <div class="card">
+        <div class="card-header">
+          <h6 class="mb-0">Contact form</h6>
+        </div>
+        <div class="card-body">
+          <script src="/assets/contact-form.js" async defer></script>
+          <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback" async defer nonce="<?=$recaptcha['nonce']?>"></script>
+          <form action="/engineer/contact" method="POST" autocomplete="on" class="needs-validation" novalidate>
+            <div class="mb-3">
+              <label for="name" class="col-form-label">Name</label>
+              <input type="text" class="form-control" id="name" name="message[name]" value="<?=$_POST['message']['name'] ?? ''?>" required>
+              <div class="invalid-feedback">
+                This field cannot be blank.
+              </div>
             </div>
-          </div>
-          <div class="mb-3">
-            <label for="email" class="col-form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="message['email']" maxlength="254" required>
-            <div class="invalid-feedback">
-              This field cannot be blank and must be a valid email address.
+            <div class="mb-3">
+              <label for="email" class="col-form-label">Email</label>
+              <input type="email" class="form-control" id="email" name="message[email]" value="<?=$_POST['message']['email'] ?? ''?>" maxlength="254" required>
+              <div class="invalid-feedback">
+                This field cannot be blank and must be a valid email address.
+              </div>
+              <div class="form-text">
+                This will only be used to forward you a copy of your message and so I can reply to your message. I'll never share your email with anyone.
+              </div>
             </div>
-            <div class="form-text">
-              This will only be used to forward you a copy of your message and so I can reply to your message. I'll never share your email with anyone.
+            <div class="mb-3">
+              <label for="message" class="form-label">Message</label>
+              <textarea class="form-control" id="message" name="message[message]" rows="3" required autocomplete="off"><?=$_POST['message']['message'] ?? ''?></textarea>
+              <div class="invalid-feedback">
+                This field cannot be blank.
+              </div>
             </div>
-          </div>
-          <div class="mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea class="form-control" id="message" name="message['message']" rows="3" required autocomplete="off"></textarea>
-            <div class="invalid-feedback">
-              This field cannot be blank.
+            <div class="mb-3">
+              <div class="g-recaptcha" data-sitekey="<?=$recaptcha['sitekey']?>" data-callback="successfulResponseCallback"></div>
+              <div class="invalid-feedback">
+                The reCAPTCHA box must be checked.
+              </div>
             </div>
-          </div>
-          <div class="mb-3">
-            <div class="g-recaptcha" data-sitekey="<?=$recaptcha['sitekey']?>" data-callback="successfulResponseCallback"></div>
-            <div class="invalid-feedback">
-              The reCAPTCHA box must be checked.
-            </div>
-          </div>
-          <input class="btn btn-primary" type="submit" value="Submit">
-        </form>
+            <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+          </form>
+        </div>
       </div>
-    </div>
+    <?php } ?>
   </div>
   <div class="col-xl-4">
     <div class="card mb-3">
