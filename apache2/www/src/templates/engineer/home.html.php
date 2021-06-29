a19e1fa09e9598ae256e5ff4ae0343635628968c
relnaggar <rmsy@rmsy.me>
2021-06-29 19:21:52 +0100

    [RM-15] add introduction, hero shot and static project cards

--- apache2/www/src/templates/engineer/home.html.php
+++ apache2/www/src/templates/engineer/home.html.php
@@ -1,4 +1,45 @@
-<p class="lead">This is the home page for the software engineering part of the site. This website is under construction.</p>
-<?php for ($i = 0; $i < 3; $i++) { ?>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At varius vel pharetra vel turpis nunc. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Sit amet mauris commodo quis imperdiet massa. Porta nibh venenatis cras sed felis eget velit aliquet sagittis. Arcu ac tortor dignissim convallis aenean et. Sed felis eget velit aliquet sagittis id consectetur. Sit amet luctus venenatis lectus magna fringilla. Libero justo laoreet sit amet cursus sit. Praesent tristique magna sit amet purus gravida quis blandit turpis. Phasellus vestibulum lorem sed risus ultricies tristique. Lacus luctus accumsan tortor posuere ac ut consequat semper. Massa sed elementum tempus egestas sed sed risus pretium.</p>
-<?php } ?>
+<p class="lead">
+  Welcome to the home page of my personal website.
+</p>
+
+<p>
+  Hi, I'm Ramsey and I'm a software engineer. I built this website from scratch to showcase some of the skills I've developed after studying informatics for five years and then teaching the subject privately for a further three years.
+</p>
+
+<p class="text-center">
+  <a href="/engineer/about" class="btn btn-primary">Read my story</a>
+</p>
+
+<div class="card mb-3">
+  <img src="/assets/img/hero-shot.jpg" class="w-100" alt="Hero shot">
+  <div class="card-body">
+    <p class="card-text text-center">Me (left) restarting the robot I built for my Master's project (right). We're trying to outsmart the dung beetle.</p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6 mb-3">
+    <div class="card">
+      <!--<img src="" class="card-img-top" alt="Personal Website project main image">-->
+      <div class="card-body">
+        <h5 class="card-title">Personal Website project</h5>
+        <p class="card-text">
+          The first draft of this website is now complete.
+        </p>
+        <a href="/engineer/website/" class="btn btn-primary">Find out more</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <!--<img src="" class="card-img-top" alt="project main image">-->
+      <div class="card-body">
+        <h5 class="card-title">More projects to be uploaded</h5>
+        <p class="card-text">
+          ...
+        </p>
+        <a class="btn btn-primary disabled">Coming soon</a>
+      </div>
+    </div>
+  </div>
+</div>
