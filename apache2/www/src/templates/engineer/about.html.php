c6ac65a4647271a9d73fcccdccfc77cf1e0dd575
relnaggar <rmsy@rmsy.me>
2021-10-09 19:18:08 +0100

    fix date on about page

--- apache2/www/src/templates/engineer/about.html.php
+++ apache2/www/src/templates/engineer/about.html.php
@@ -1,5 +1,5 @@
 <p class="lead">I'm still crafting the story I want to publish here.</p>
 <p>In the meantime, you could check out <a href="/linkedin" target="_blank">my LinkedIn profile</a> <?=includeIcon('box-arrow-up-right')?>. It's due an update and some streamlining, but you'll find all of the main highlights of my career so far.
 <blockquote class="blockquote pt-3">
-<p class="blockquote-footer">Ramsey El-Naggar, June 2020</p>
+<p class="blockquote-footer">Ramsey El-Naggar, June 2021</p>
 </blockquote>
