fa2cf72789c44895825746c6bcbdf5b081def960
relnaggar <rmsy@rmsy.me>
2023-06-18 19:40:51 +0100

    add navigation section to back-end write-up

---
+++ apache2/www/src/templates/website/back/navigation.html.php
@@ -0,0 +1,17 @@
+<p>
+  Like a trail of digital breadcrumbs, the nifty menu and sidebar system is your friendly neighbourhood guide through the labyrinth of this website.
+  The first thing you'll notice is the menu bar at the top of the page.
+  Look at that beauty!
+  And like all the best menus, it highlights where you currently are, ensuring you don't accidentally get sucked into the dark web -- watch your step there!
+  Same for the sidebar and the 'On this page' menu, all of which are automatically generated for your convenience.
+  Not to mention the 'Previous' and 'Next' buttons, which act as your digital compass, guiding you on your epic adventure and making sure you get back home in time for dinner.
+</p>
+
+<p>
+  But for me as a developer, the best part of this whole contraption is that it's all automated, making it easy (maybe a little too easy) to add new sections, pages and even projects.
+  For example, if I want to add a new project, all I have to do is add it to the <a href="#framework">routing system</a>, create a new directory in the <a href="#templating">template engine</a> and make another <a href="#controllers">controller</a>.
+  The rest is taken care of by the automation fairies.
+  Aren't they just swell?
+  And this then allows me to focus on the most important part of this whole endeavour: writing ridiculous programming metaphors.
+  Yeehaw!
+</p>
