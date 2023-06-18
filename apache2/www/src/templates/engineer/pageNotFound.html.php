99b2f8e8bf97ce8cad16274e6eb32863f468fd41
relnaggar <rmsy@rmsy.me>
2023-06-18 04:06:43 +0100

    add funny 404 page

--- apache2/www/src/templates/engineer/pageNotFound.html.php
+++ apache2/www/src/templates/engineer/pageNotFound.html.php
@@ -1,1 +1,19 @@
-<p class="lead">The page requested has not been found.</p>
+<p class="lead">Oh No! It's a 404!</p>
+
+<p>
+Looks like you've boldly gone where no link has gone before.
+The page you're looking for is out of service, or maybe it's just hiding from your prying eyes.
+Who knows?
+</p>
+
+<p>
+Meanwhile, the web gnomes are furiously running around, pretending to fix this.
+</p>
+
+<p>
+While they're at it, why not teleport back to <a href="/engineer/">home</a>?
+</p>
+
+<p>
+Remember, it's not you, it's me. Well, mostly me. Probably.
+</p>
