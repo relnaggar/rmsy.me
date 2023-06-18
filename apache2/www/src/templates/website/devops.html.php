6a4f0fba5e2139d1c5e959bf136bbeecbed5ff1b
relnaggar <rmsy@rmsy.me>
2023-06-18 20:45:44 +0100

    add basic devops page

---
+++ apache2/www/src/templates/website/devops.html.php
@@ -0,0 +1,17 @@
+<p>
+  <div class="alert alert-info" role="alert">
+    I'm still writing up this project: I've not yet uploaded the content for this page.
+  </div>
+</p>
+
+<h6>Achievements</h6>
+<ul>
+  <li>Set up and configured the AWS cloud server instance.</li>
+  <li>Installed and securely configured the Apache HTTP Server software.</li>
+  <li>Organised the application into standalone Docker containers.</li>
+  <li>Configured the containers to work together.</li>
+  <li>Wrote Bash scripts to automate development, testing, staging and deployment as far as possible.</li>
+  <li>Created and configured a database service for the application.</li>
+  <li>Added unit testing framework PHPUnit.</li>
+  <li>Integrated end-to-end testing framework Cucumber including browser automation with Selenium.</li>
+</ul>
