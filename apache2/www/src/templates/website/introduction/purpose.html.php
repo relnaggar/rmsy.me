eb2a02331250fa301e03f62faeb278c31e57dfdf
relnaggar <rmsy@rmsy.me>
2023-06-18 04:09:06 +0100

    update website introduction

--- apache2/www/src/templates/website/introduction/purpose.html.php
+++ apache2/www/src/templates/website/introduction/purpose.html.php
@@ -1,14 +1,27 @@
 <p class="lead mt-3">
   This project is the website you are viewing now!
 </p>
+
 <p>
-  The main purpose of the project is to showcase the web-development skills I've picked up during my career so far. 
+  Whoa there, friend!
+  You've just stepped into a wild west of web wizardry.
+  Round these parts, we don't just dabble in the digital arts, we wrangle 'em.
+  Let me spin you a tale.
 </p>
+
 <p>
-  My intention is to use this section of the website to include more information about how exactly I used this wide range of tools and technologies, and to describe the thinking process behind each of the main design decisions taken.
+  This here project is more than a cluster of coding, and I didn't just rustle it up on a whim.
+  No, sir!
+  It was born instead out of two heartfelt desires:
+  <ol>
+    <li>A need to display my professional prowess in this particular area of software engineering.</li>
+    <li>A craving for coding challenges to stretch my skills and prepare me for even trickier web projects in the future.</li>
+  </ol>
 </p>
+
 <p>
-  <div class="alert alert-info" role="alert">
-    I'm still writing up the Personal Website project: the links below and in the sidebar currently lead to empty pages.
-  </div>
+  Starting as a rough sketch in the dust, it slowly morphed into what you see before you.
+  Each byte of data shaped by my hand, each line of code carrying a piece of my story.
+  Every tool, technology, and technique I lassoed into service became part of this grand rodeo.
+  So kick off your boots, relax, and discover the essence of what makes this website tick.
 </p>
--- apache2/www/src/templates/website/introduction/skills.html.php
+++ apache2/www/src/templates/website/introduction/skills.html.php
@@ -1,13 +1,52 @@
 <p>
-  The skills involved in the creation of this website covered a diverse range of areas:
-  <dl>
-    <dt>Server administration: <a href="/engineer/website/aws">AWS</a>, <a href="/engineer/website/apache">Apache</a></dt>
-    <dd>Setting up and configuring the cloud server instance; installing and securely configuring the server software.</dd>
-    <dt>DevOps: <a href="/engineer/website/docker">Docker</a>, <a href="/engineer/website/bash">Bash</a><dt>
-    <dd>Organising the application into standalone containers; configuring the containers to work together; writing scripts to automate development, testing, staging and deployment as far as possible; creating and configuring a database service for the application; integrating unit testing and end-to-end testing with browser automation.</dd>
-    <dt>Front-end development: <a href="/engineer/website/bootstrap">Bootstrap</a></dt>
-    <dd>Designing a responsive front-end for the application.</dd>
-    <dt>Back-end development: <a href="/engineer/website/php">PHP</a></dt>
-    <dd>Building a back-end PHP framework from scratch; creating a spam-free contact form.</dd>
+  Building this website was like a midnight horse ride through the ever-changing landscape of software technologies.
+  Sometimes, it was just a quick pit stop -- like when I had to get my hands dirty with npm (Node Package Manager) to nurse a broken <code>package.json</code> file back to health.
+  Other times it was a long haul -- like that time when Dockerfiles and I had a two-week greasy wrestling marathon under the starlit skies of sleepless nights.
+  And of course there were those times when the journey wasn't a gallop but a gentle canter -- like that glorious summer spent exploring Italy whilst learning PHP in my spare time, every stolen moment spent chipping away like Michelangelo letting his sculpture emerge slowly from a block of marble.
+</p>
+
+<p>
+  Now, after the dust has settled, I've arrived at my destination a battle-tested software soldier, trained and confident in the following weapons of mass construction:
+</p>
+
+<ul>
+  <li>Docker</li>
+  <li>Bash</li>
+  <li>Git</li>
+  <li>Amazon Web Services (AWS)</li>
+  <li>Apache HTTP Server</li>
+  <li>PHP</li>
+  <li>HTML5/CSS3</li>
+  <li>Bootstrap</li>
+</ul>
+
+<p>  
+  But wait!
+  I hear you ask, "Which skills does this coding cowboy particularly want to emphasise?"
+  Well, partner, I'm glad you asked!
+  My talents tend to sip from three main watering holes:
+  <dl>    
+    <dt><a href="/projects/website/devops">DevOps</a></dt>
+    <dd>
+    Taming Docker to containerise the application, building an automated DevOps pipeline using Bash scripts and Git hooks, and manually tuning the AWS EC2 instance and Apache HTTP Server like a trusty old guitar.
+    </dd>
+
+    <dt><a href="/projects/website/back">Back-end development</a></dt>
+    <dd>
+      Constructing a PHP back-end framework from scratch, complete with routing, templating, and a spam-free contact form so airtight that not even the Sam Fisher of stealthy spambots could sneak past.
+    </dd>
+    
+    <dt><a href="/projects/website/front">Front-end</a></dt>
+    <dd>
+      Crafting an intricate front-end HTML/CSS template with Bootstrap.
+      You see that fully responsive menu above, sidebar to the left and table of contents to the right?
+      That's right, I built that.
+      (If you're on a mobile or small screen, you'll see them compressed at the top instead).
+    </dd>
   </dl>
+</p>
+
+<p>
+  So, saddle up and stick around, partner.
+  There's plenty more to explore in this digital wild west!
 </p>
