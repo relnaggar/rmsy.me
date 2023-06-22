0bf1d16dd5255f86541e0e2df2c62d982a441658
relnaggar <rmsy@rmsy.me>
2023-06-22 02:38:45 +0100

    add SEO to front-end write-up

--- apache2/www/src/templates/website/front.html.php
+++ apache2/www/src/templates/website/front.html.php
@@ -22,7 +22,7 @@
         Ever tried to fit a sumo wrestler into skinny jeans? No? Me neither.
         But that's the kind of flexibility we're talking about here.
         This website is a digital gymnast -- it bends, stretches, and contorts itself to fit whatever screen size you're using.
-        It's all thanks to the magic of Bootstrap, which is basically the website equivalent of free yoga lessons.
+        It's all thanks to the magic of Bootstrap, which is the website equivalent of free yoga lessons.
         Fancy a little demo?
         Go ahead, play with the size of your browser window.
       </p>
@@ -35,10 +35,36 @@
         Sit back and enjoy the spectacle!
       </p>
 
-      <figure class="figure m-3 text-center col-lg-5 w-100">
-        <img src="/assets/img/responsive.gif" class="figure-img img-fluid" alt="Responsive design" />
+      <figure class="figure m-3 text-center w-100">
+        <img src="/assets/img/responsive.gif" class="figure-img img-fluid" alt="Responsive design">
         <figcaption class="figure-caption text-center">Definitive proof that (screen) size doesn't matter.</figcaption>
       </figure>
+    </dd>
+
+    <dt>Getting into Google's good books</dt>
+    <dd>
+    I've taken steps to ensure that the people who want to find me can find me.
+    By taking a deep dive into the world of SEO, I've made this website a veritable beacon in the night.
+    From <a href="/robots.txt"><code>robots.txt</code></a> and <a href="/sitemap.xml"><code>sitemap.xml</code></a>, to strategically planting <code>meta</code> <code>description</code> tags on every page, and carefully placing <code>meta</code> <code>noindex</code> tags and <code>data-nosnippet</code> attributes to puppeteer Google's descriptions of my site, every detail was fine-tuned for maximum visibility.
+    The result?
+    My site landing on the coveted first page for most combinations of my first name and profession.
+    Try it out yourself!
+    </dd>
+
+    <figure class="figure m-3 text-center w-100">
+      <img src="/assets/img/seo.png" class="figure-img img-fluid" alt="SEO demonstration">
+      <figcaption class="figure-caption text-center">
+        I searched for "ramsey software engineer" on an incognito window to avoid personalized results.
+      </figcaption>
+    </figure>
+
+    <dt>Meticulous attention to detail</dt>
+    <dd>
+      You know that satisfaction when every single pixel is exactly where it should be?
+      That's my jam.
+      Every alignment, every color choice, every font choice and every menu item –- it's all been carefully thought out and crafted to create an immersive, harmonious user experience.
+      It's not just about looking good –- it's about feeling right.
+      My obsession with detail might verge on neurotic, but hey, when the result is a website this slick, I'm okay with that.
     </dd>
 
     <dt>The fact that I did it all myself</dt>
