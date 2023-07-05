3acfee62a85a4b9476670346f3cb352dd8481d73
relnaggar <rmsy@rmsy.me>
2023-07-05 13:44:28 +0100

    Change some wording in front-end write-up

--- apache2/www/src/templates/website/front.html.php
+++ apache2/www/src/templates/website/front.html.php
@@ -1,6 +1,6 @@
 <p>
   Roll into the digital dining room that is the front-end of this website.
-  Savour the ambiance, admire the decor, peruse the menu, and hey, don't forget to wink at that charming server (spoiler alert: that's me!).
+  Savour the ambiance, admire the decor, peruse the menu, and hey, don't forget to wink at that charming server (that's me!).
   In website terms, the front-end includes the layout, the colors, the buttons, the images, and even the way things move around.
   It's all about that sweet sweet user experience. Mmmmm, delicious.
   So take a second to look around and appreciate the fine dining aesthetics.
@@ -54,7 +54,7 @@
     <figure class="figure m-3 text-center w-100">
       <img src="/assets/img/seo.png" class="figure-img img-fluid" alt="SEO demonstration">
       <figcaption class="figure-caption text-center">
-        I searched for "ramsey software engineer" on an incognito window to avoid personalized results.
+        I searched for "ramsey software engineer" in a new incognito window to avoid personalised results from the Google spymasters.
       </figcaption>
     </figure>
 
@@ -62,8 +62,8 @@
     <dd>
       You know that satisfaction when every single pixel is exactly where it should be?
       That's my jam.
-      Every alignment, every color choice, every font choice and every menu item –- it's all been carefully thought out and crafted to create an immersive, harmonious user experience.
-      It's not just about looking good –- it's about feeling right.
+      Every alignment, every color and font choice, every menu item –- it's all been carefully thought out and crafted to create an immersive, harmonious user experience.
+      It's not just about looking good, it's about feeling right.
       My obsession with detail might verge on neurotic, but hey, when the result is a website this slick, I'm okay with that.
     </dd>
 
