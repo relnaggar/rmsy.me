8cc2ffe321ef3f40318981016d3a0c4106fdae48
relnaggar <rmsy@rmsy.me>
2023-07-07 01:57:12 +0100

    Add selenium gif

---
+++ apache2/www/src/templates/website/devops/testing.html.php
@@ -0,0 +1,4 @@
+<figure class="figure m-3 text-center w-100">
+  <img src="/assets/img/selenium.gif" class="figure-img img-fluid" alt="TODO">
+  <figcaption class="figure-caption text-center">TODO.</figcaption>
+</figure>
