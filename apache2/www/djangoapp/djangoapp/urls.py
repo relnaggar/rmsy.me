9624a302bc2b53afd5a23d2432e0f431add1a6a7
relnaggar <rmsy@rmsy.me>
2023-08-16 21:41:44 +0100

    set up jobhunting app

--- apache2/www/djangoapp/djangoapp/urls.py
+++ apache2/www/djangoapp/djangoapp/urls.py
@@ -24,6 +24,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("polls.urls")),
+    path("", include("jobhunting.urls")),
     path("admin/", admin.site.urls),
 ]
