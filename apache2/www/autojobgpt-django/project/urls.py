ef95009f3ff99fd2b19ead21ff776f8dbe03eb1a
relnaggar <rmsy@rmsy.me>
2023-08-20 05:46:12 +0100

    add link to API root

--- apache2/www/autojobgpt-django/project/urls.py
+++ apache2/www/autojobgpt-django/project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [    
-  path('admin/', admin.site.urls, name="admin"),
+  path('admin/', admin.site.urls),
   path('api-auth/', include('rest_framework.urls')),
   path("", include("app.urls")),
 ]
