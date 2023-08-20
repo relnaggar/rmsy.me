419c01ea6a32eeece2cae532447e3fa957c5204d
relnaggar <rmsy@rmsy.me>
2023-08-20 20:51:45 +0100

    add django media directory

--- apache2/www/autojobgpt-django/project/settings.py
+++ apache2/www/autojobgpt-django/project/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/public/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/public/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
