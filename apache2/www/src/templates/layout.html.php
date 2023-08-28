c87d6b3c7410b6a1508685f7bf5971d46f85caf3
relnaggar <rmsy@rmsy.me>
2023-08-28 12:45:53 +0100

    add XHTML close tags

--- apache2/www/src/templates/layout.html.php
+++ apache2/www/src/templates/layout.html.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <?php require 'layout/head.html.php'; ?>
   <?php require 'layout/body.html.php'; ?>
 </html>
--- apache2/www/src/templates/layout/head.html.php
+++ apache2/www/src/templates/layout/head.html.php
@@ -1,15 +1,16 @@
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="/assets/custom.bootstrap.min.css">
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />  
+  <link rel="stylesheet" type="text/css" href="/assets/custom.bootstrap.min.css" />
 
   <title><?=$meta['title']?> | <?php if (isset($sidebar)) { ?><?=$sidebar->getTitle()?> | <?php } ?>Ramsey El-Naggar</title>
 
   <?php if (isset($meta['description'])) { ?>
-    <meta name="description" content="<?=$meta['description']?>">
+    <meta name="description" content="<?=$meta['description']?>" />
   <?php } ?>
 
   <?php if (isset($meta['noindex']) && $meta['noindex']) { ?>
-    <meta name="robots" content="noindex">
+    <meta name="robots" content="noindex" />
   <?php } ?>
 </head>
