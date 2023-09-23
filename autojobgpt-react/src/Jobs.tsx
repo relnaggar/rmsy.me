03450d5671df0c40cbc70f0d654aae791845ca4a
relnaggar <rmsy@rmsy.me>
2023-09-23 20:44:16 +0100

    add kanban layout

--- autojobgpt-react/src/Jobs.tsx
+++ autojobgpt-react/src/Jobs.tsx
@@ -1,10 +1,56 @@
-import { JOBS } from './api';
+import { JOBS, Job, STATUSES } from './api';
+
+function capitaliseFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 export default function Jobs(): JSX.Element {
   return (
-    <>
-      <h1>Jobs</h1>
-      <p>Jobs page</p>
-    </>
+    <main>
+      <Board columnTitles={STATUSES} jobs={JOBS}/>
+    </main>
+  );
+}
+
+function Board({ columnTitles, jobs }: {columnTitles: string[], jobs: Job[]}): JSX.Element {
+  return (
+    <div className="kanban-board border">
+      {columnTitles.map((columnTitle) => {
+        return (
+          <Column key={columnTitle} title={columnTitle} jobs={
+            jobs.filter((job) => (job.status === "" && columnTitle === "backlog") || (job.status === columnTitle))
+          } />
+        );
+      })}
+    </div>
+  );
+}
+
+function Column({ title, jobs }: {title: string, jobs: Job[]}): JSX.Element {
+  return (
+    <div className="kanban-column me-2">
+      <div className="card">
+        <div className="card-header">
+          <h5 className="card-title">{capitaliseFirstLetter(title)}</h5>
+        </div>  
+        <div className="card-body">          
+          {jobs.map((job) => {
+            return <JobCard job={job} key={job.id} />
+          })}
+        </div>
+        <div className="card-footer">
+          { title === "backlog" ? "+ add new job" : "" }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function JobCard({ job }: {job: Job}): JSX.Element {
+  return (
+    <div className="card mb-2 p-2">
+      <h6 className="card-title">{job.title}</h6>
+      <p className="card-subtitle">{job.company}</p>
+    </div>
   );
 }
--- autojobgpt-react/src/Layout.tsx
+++ autojobgpt-react/src/Layout.tsx
@@ -4,22 +4,23 @@ import { Outlet, Link } from "react-router-dom";
 export default function Layout(): JSX.Element {
   return (
     <div className="container">
-      <nav className="navbar navbar-expand bg-body-tertiary sticky-top">
-        <div className="container-fluid">
+
+      <nav className="navbar navbar-expand bg-body-tertiary">
+        <div className="container-fluid justify-content-start">
           <Link to="/" className="navbar-brand">AutoJobGPT</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/jobs" className="nav-link">Jobs</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/resumes" className="nav-link">Resumes</Link>
-              </li>
-            </ul>
-          </div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/jobs" className="nav-link">Jobs</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/resumes" className="nav-link">Resumes</Link>
+            </li>
+          </ul>
         </div>
       </nav>
+
       <Outlet />
+
     </div>
   )
 }
