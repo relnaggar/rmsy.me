4fa933c68d91366a5115126afe380f354a1e5d05
relnaggar <rmsy@rmsy.me>
2023-11-16 01:08:24 +0000

    add better logging, including django

--- apache2/www/autojobgpt-django/project/settings.py
+++ apache2/www/autojobgpt-django/project/settings.py
@@ -142,3 +142,33 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/apache2/django.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'app': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
