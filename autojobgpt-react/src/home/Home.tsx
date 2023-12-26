7cbab9e1b06c6cf9d57ff868a13cdb35a7404f8e
relnaggar <rmsy@rmsy.me>
2023-12-26 20:35:54 +0000

    add basic home page content

--- autojobgpt-react/src/home/Home.tsx
+++ autojobgpt-react/src/home/Home.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Home = (): React.JSX.Element => {
   return (
-    <h2 className="mb-3">Home</h2>
+    <>
+      <h2 className="mb-3">Home</h2>
+      <p className="lead">Welcome to AutoJobGPT!</p>
+      <p>
+        This is a simple web app that allows you to automate the process of tailoring a resume or cover letter to a job description.
+      </p>
+      <p>
+        To get started, go to the <Link to="/jobs">Jobs</Link> page.
+      </p>
+    </>
   );
 };
 
