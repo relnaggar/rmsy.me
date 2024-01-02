11b0296fca7ec2f1529699351917035d804aef71
relnaggar <rmsy@rmsy.me>
2024-01-02 19:41:46 +0000

    swap resume bullet point order

--- apache2/www/src/templates/autojobgpt/introduction/summary.html.php
+++ apache2/www/src/templates/autojobgpt/introduction/summary.html.php
@@ -4,9 +4,10 @@
       Developed job application kanban board with React TypeScript, Bootstrap, and Django REST Framework.
     </li>
     <li>
-      Implemented .docx file handling for template uploads and allowed user customisation of uploaded templates.</li>
-    <li>
       Leveraged OpenAI API to pioneer AI-driven automatic tailoring of resumes and cover letters to each job.
+    </li>
+    <li>
+      Implemented .docx file handling for template uploads and allowed user customisation of uploaded templates.
     </li>
   </ul>
 </p>
