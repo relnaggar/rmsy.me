93223d0558f2f79d00bca9d0424df547932aa910
relnaggar <ramsey.el-naggar@outlook.com>
2024-10-27 12:57:43 +0000

    FIX preloading bootstrap icons

--- www/html/index.php
+++ www/html/index.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="preload" href="fonts/bootstrap-icons.woff2" as="font" type="font/woff2">
-    <link rel="preload" href="fonts/bootstrap-icons.woff" as="font" type="font/woff">
+    <link
+      rel="preload"
+      href="fonts/bootstrap-icons.woff2?24e3eb84d0bcaf83d77f904c78ac1f47"
+      as="font"
+      type="font/woff2"
+      crossorigin="anonymous"
+    >
     <title>Hello World</title>
   </head>
   <body>
