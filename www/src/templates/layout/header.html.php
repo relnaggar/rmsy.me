7fd5fe24841ca67cf08f1c05acac20ee2428974b
relnaggar <ramsey.el-naggar@outlook.com>
2024-11-02 01:53:26 +0000

    redesign header

--- www/src/templates/layout/header.html.php
+++ www/src/templates/layout/header.html.php
@@ -1,18 +1,24 @@
 <header class="text-center">
-  <div class="display-none-md-down d-flex">
+  <div class="display-none-xl-down d-flex">
     <a
       href="/"
-      class="display-4 flex-fill text-reset bg-contrast-primary left-banner"
+      class="display-5 flex-fill text-reset bg-contrast-primary banner-tag"
     >
       &lt;<b>r</b>a<b>ms</b>e<b>y</b><b class="banner-link">.me</b>&gt;
     </a>
-    <span class="display-none-md-down display-4 flex-fill" id="center-banner">
+    <span class="display-none-md-down display-5 flex-fill" id="center-banner">
       Ramsey El&#8209;Naggar
     </span>
+    <a
+      href="/"
+      class="display-5 flex-fill text-reset bg-contrast-primary banner-tag"
+    >
+      &lt;/<b>r</b>a<b>ms</b>e<b>y</b><b class="banner-link">.me</b>&gt;
+    </a>
   </div>
-  <div class="display-none-md-up">
-    <a href="/" class="display-2 text-reset left-banner">
-      &lt;<b>rmsy</b><b class="banner-link">.me</b>&gt;
+  <div class="display-none-xl-up p-2">
+    <a href="/" class="display-5 text-reset banner-tag">
+      &lt;<b>rmsy</b><b class="banner-link">.me</b>/&gt;
     </a>
   </div>
 </header>
