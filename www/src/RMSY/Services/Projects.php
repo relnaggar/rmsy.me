8d9c075bfad4111c2b86073a2a252e29d13d902c
relnaggar <ramsey.el-naggar@outlook.com>
2024-11-02 19:00:22 +0000

    add beetle project

--- www/src/RMSY/Services/Projects.php
+++ www/src/RMSY/Services/Projects.php
@@ -7,6 +7,26 @@ class Projects extends \Framework\Services\AbstractService {
       'beetle' => [
         'title' => 'Pooptimus Prime: The World\'s First Dung Beetle Biorobot',
         'description' => '',
+        'sections' => [
+          'summary' => [
+            'title' => 'Executive Summary',
+          ],
+          'beetles101' => [
+            'title' => 'The Beetle Hunter Diaries',
+          ],
+          'robot' => [
+            'title' => 'Say Hello to My Little Friend',
+          ],
+          'brain' => [
+            'title' => 'Inside the Beetle Brain: It\'s Not All Dung and Games',
+          ],
+          'cues' => [
+            'title' => 'Visual Cue Integration: The Sky\'s the Limit',
+          ],
+          'southAfrica' => [
+            'title' => 'Rolling in the Deep: A South African Saga',
+          ],
+        ],
       ],
       'sdp' => [
         'title' => 'Robo-Messi: When Silicon Met Soccer',
