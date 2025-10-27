bb422163569256f6c041c56e55796f748bc8fc21
relnaggar <ramsey.el-naggar@outlook.com>
2025-10-27 12:40:27 +0000

    add payments table

--- migrations/20251017091716_create_users_table.php
+++ migrations/20251017091716_create_users_table.php
@@ -19,11 +19,20 @@ final class CreateUsersTable extends AbstractMigration
      */
     public function change(): void
     {
+        // <<<SQL
+        // CREATE TABLE users (
+        //     id INT PRIMARY KEY AUTO_INCREMENT,
+        //     email VARCHAR(254) NOT NULL UNIQUE,
+        //     password_hash VARCHAR(64) NOT NULL,
+        //     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        //     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        // );
+        // SQL;
         $this->table('users')
-            ->addColumn('email', 'string', ['limit' => 254])
-            ->addColumn('password_hash', 'string', ['limit' => 64])
-            ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
-            ->addColumn('updated_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('email', 'string', ['limit' => 254, 'null' => false])
+            ->addColumn('password_hash', 'string', ['limit' => 64, 'null' => false])
+            ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP', 'null' => false])
+            ->addColumn('updated_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP', 'null' => false])
             ->addIndex(['email'], ['unique' => true])
             ->create();
     }
---
+++ migrations/20251027121133_create_payments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreatePaymentsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        // <<<SQL
+        // CREATE TABLE payments (
+        //     id VARCHAR(40) PRIMARY KEY,
+        //     datetime DATETIME NOT NULL,
+        //     amount INT NOT NULL,
+        //     currency VARCHAR(3) NOT NULL,
+        //     payment_reference VARCHAR(150) NOT NULL,
+        //     payer_name VARCHAR(100) NOT NULL
+        // );
+        // SQL;
+
+        $this->table('payments', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'string', ['limit' => 40, 'null' => false])
+            ->addColumn('datetime', 'datetime', ['null' => false])
+            ->addColumn('amount', 'integer', ['null' => false])
+            ->addColumn('currency', 'string', ['limit' => 3, 'null' => false])
+            ->addColumn('payment_reference', 'string', ['limit' => 150, 'null' => false])
+            ->addColumn('payer_name', 'string', ['limit' => 100, 'null' => false])
+            ->create();
+    }
+}
